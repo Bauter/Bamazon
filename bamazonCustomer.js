5969d4d0fbecd104c43dd10057ba94b2f3cb4071
Bauter <bauter09@gmail.com>
2020-03-03 19:20:27 -0600

    inquirer functions now working for item selection. total and shopping cart logged, need to find a way to add elements in shoppingCartTotal together, mysql still needs update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,11 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 
+//global variables
+let total;
+let shoppingCart = [];
+let shoppingCartTotal = [];
+
 // Settign up mysql connection 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -29,5 +34,75 @@ function showItems() {
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     console.table(res);
+    startInquirer(res);
+  });
+};
+
+function startInquirer(res) {
+  let itemsArray = res;
+  inquirer.prompt([
+    {
+      type:"input",
+      name:"itemSelect",
+      message:"Please input the 'item_id' of the item you would like to purchase",
+      default: 1
+    },
+    {
+      type:"input",
+      name:"itemQuantity",
+      message:"How many would you like?",
+      default: 1
+      
+    }
+  ]).then(function(response) {
+   
+    let itemID = response.itemSelect;
+    let numOf = response.itemQuantity;
+
+    for (i=0; i< itemsArray.length; i++) {
+      
+      if (itemID == itemsArray[i].item_id) {
+        console.log("match found! " + itemsArray[i].product_name)
+        shoppingCart.push(" " + itemsArray[i].product_name);
+        if (numOf <= itemsArray[i].stock_quantity) {
+          console.log("ok, ordering " + numOf + " " + itemsArray[i].product_name )
+          total = itemsArray[i].price * numOf
+          shoppingCartTotal.push(" " + total);
+        } else {
+          console.log("Sorry, quantity of that item is limited.")
+        };
+      } else {
+        console.log("no item by that item_id");
+        
+      }
+    }; // end of for loop
+
+    console.log("Shopping cart: " + shoppingCart);
+    console.log("total for this order: " + total);
+    console.log("total for all items in shopping cart: " + shoppingCartTotal);
+    // update database before asking customer if they want to keep shopping
+    confirm();
   })
+
+
+
+
 }
+
+// Prompt the user to see if they would like to keep shopping
+function confirm() {
+  inquirer.prompt([
+    {
+      type:"confirm",
+      name:"confirm",
+      message:"Would you like to keep shopping?"
+    }
+  ]).then(function(response) {
+    if (response.confirm === true) {
+      showItems();
+    } else {
+      process.exit;
+    }
+
+  });
+};
